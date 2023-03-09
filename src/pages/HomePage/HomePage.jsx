578d62f2d0b30fdd06e6d0b9d1f49a96bdb09085
Nikimad <PybePoiD@gmail.com>
2023-03-09 13:11:41 +0300

    Add HomePage page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "../../components/Wrapper";
+import Aside from "../../components/Aside";
+
+const HomePage = () => (
+  <Wrapper>
+    <Aside />
+  </Wrapper>
+);
+
+export default HomePage;
